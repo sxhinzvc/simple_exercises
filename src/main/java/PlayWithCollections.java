f96221cf024c1e68dc97cc326e98ffacb2632cba
Julia Lee <julee@thoughtworks.com>
2016-08-12 15:20:18 -0500

    added class for Collections & Map implementations. added README

---
+++ src/main/java/PlayWithCollections.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class PlayWithCollections {
+
+    List<String> arrayList = new ArrayList<>();
+    List<String> vector = new Vector<>();
+    List<String> linkedList = new LinkedList<>();
+
+    Set<String> hashSet = new HashSet<>();
+    Set<String> linkedHashSet = new LinkedHashSet<>();
+    Set<String> treeSet = new TreeSet<>();
+
+    Queue<String> priorityQueue = new PriorityQueue<>();
+
+    Map<Integer, String> hashMap = new HashMap<>();
+    Map<Integer, String> linkedHashMap = new LinkedHashMap<>();
+    Map<Integer, String> treeMap = new TreeMap<>();
+}
